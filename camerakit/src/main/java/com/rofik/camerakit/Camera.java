38c0010f08ec98a4d080c4a73d6d10d600f509c5
rofik <rofik1075@gmail.com>
2022-05-19 14:41:22 +0700

    add pick from gallery

--- camerakit/src/main/java/com/rofik/camerakit/Camera.java
+++ camerakit/src/main/java/com/rofik/camerakit/Camera.java
@@ -5,20 +5,23 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
-import android.widget.Toast;
+
+import java.io.IOException;
+
 import pub.devrel.easypermissions.EasyPermissions;
 
 import static android.app.Activity.RESULT_OK;
-import static androidx.core.app.ActivityCompat.startActivityForResult;
 
 public class Camera {
     Context context;
     Activity activity;
     private static final int RC_CAMERA_PERM = 123;
+    private static final int RC_GALERI_PERM = 124;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_GALERY = 2;
 
     public Camera(Context context, Activity activity) {
         this.context = context;
@@ -36,16 +39,33 @@ public class Camera {
         }
     }
 
+    public void requestGalery(){
+        if (hasGaleryPermission()){
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            photoPickerIntent.setType("image/*");
+            activity.startActivityForResult(photoPickerIntent, REQUEST_GALERY);
+        }else {
+            EasyPermissions.requestPermissions(activity, "Izinkan Perizinan Storage", RC_GALERI_PERM, Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+    }
+
+    private boolean hasGaleryPermission(){
+        return EasyPermissions.hasPermissions(context, Manifest.permission.READ_EXTERNAL_STORAGE);
+    }
+
     private boolean hasCameraPermission() {
         return EasyPermissions.hasPermissions(context, Manifest.permission.CAMERA);
     }
 
-    public Bitmap onActivityResult(int requestCode, int resultCode, Intent data) {
+    public Bitmap onActivityResult(int requestCode, int resultCode, Intent data) throws IOException {
         Bitmap bitmap = null;
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             assert extras != null;
             bitmap = (Bitmap) extras.get("data");
+        }else if (requestCode == REQUEST_GALERY && resultCode == RESULT_OK){
+            Uri selectedImage = data.getData();
+            bitmap = MediaStore.Images.Media.getBitmap(activity.getContentResolver(), selectedImage);
         }
         return bitmap;
     }
